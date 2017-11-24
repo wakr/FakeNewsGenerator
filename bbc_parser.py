1da145ec6b57d30c7eb0cf1456d48b195ff3e80a
Pihla Toivanen <toivanenpihla@gmail.com>
2017-11-24 14:23:18 +0200

    BBC data

---
+++ bbc_parser.py
@@ -0,0 +1,11 @@
+import glob
+import os
+
+result = open("whole_bbc_data.txt", "w+")
+
+for filename in glob.iglob('**/*.txt', recursive=True):
+     print(filename)
+     file = open(filename, "r")
+     text = file.read()
+     text = os.linesep.join([s for s in text.splitlines() if s])
+     result.write(text)
