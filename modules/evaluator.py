fc4d88de9db3f3f20a9cd4730282a313674668de
Pihla Toivanen <toivanenpihla@gmail.com>
2017-12-04 12:53:28 +0200

    If word founded from negative words, return max value

--- modules/evaluator.py
+++ modules/evaluator.py
@@ -9,7 +9,12 @@ class Evaluator:
         self.evaluate_value = evaluate_value
         self.nb_blobber = Blobber(analyzer=NaiveBayesAnalyzer())
         self.model = Word2Vec.load("../data/Model")
+        self.read_negative_words()
 
+    def read_negative_words(self):
+        text = open("../data/negative_word_list.txt").read()
+        self.negative_words = set(text.split("\n"))
+        print(self.negative_words)
 
     def novelty_evaluation(self, text):
         score = self.model.score([text])
@@ -17,6 +22,8 @@ class Evaluator:
 
 
     def value_evaluation(self, sentence):
+        if sentence in self.negative_words:
+            return 2.0
         blob = TextBlob(sentence)
         blob_naive_bayes = self.nb_blobber(sentence)
         polarity_tb = 1-((blob.sentiment.polarity + 1) / 2)
