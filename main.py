ae6a7cea28e4f807ea7bdd87ef72a548e7cabc41
Kristian <wakr@cs.helsinki.fi>
2017-12-11 15:20:27 +0200

    Regenerate only top candidate

--- main.py
+++ main.py
@@ -35,68 +35,19 @@ def grab_tweet():
     return re.sub(r'http\:\/\/cnn.it\S+', '', selected_tweet)
 
 def generate_text(tweet):
-    tb = TextBlob(tweet)
-    sent_gens = {}
-    # Split original tweet into sentences
-    for sentence_nr, sentence in enumerate(tb.sentences):
-        # Preprocess sentence
-        pos_targets = Preprocessor(str(sentence)).process()
-        # Generate alternatives for it
-        generation = Generator(pos_targets).generate()
-        print("\t-Starting to form combinations...")
-        generation_combinations = list(product(*generation))
-        print("\t-Formed {} different combinations".format(len(generation_combinations)))
-        res = ""
-        for sent_cands in generation_combinations:
-            res += ". ".join(sent_cands) + "\n"
-        # If res is empty generator failed to produce any output
-        if not res:
-            print('Generation has failed')
-        # Perform internal evaluation although it does not presently affect anything
-        # save that it takes a fixed size of samples from generated
-        sampled = internal_evaluation(res, str(sentence))
-        # Add original sentence and its variants for tweet reconstruction
-        # List index is sentence place in tweet and tuple contains original sentence
-        # plus its generated sentences
-        sampled = [sample[0] for sample in sampled]
-        sent_gens[sentence_nr] = (str(sentence), sampled)
+    pos_targets = Preprocessor(tweet).process()
+    generation = Generator(pos_targets).generate()
+    print("\t-Starting to form combinations...")
+    generation_combinations = list(product(*generation))
+    print("\t-Formed {} different combinations".format(len(generation_combinations)))
+    res = ""
 
-    gen_samples = []
-    # Recostruct new tweets from generated results
-    # NOTE: At this point reconstructing maps only one generated sentence to another
-    #       one and does not do all permutations. So if you had two sentences A and B
-    #       that produce A1, A2, B1 and B2 then output is A1 B1 and A2 B2.
-    print('\t-Reconstructing tweets')
-    for idx in range(len(sent_gens.keys())):
-        # Original sentence from tweet
-        sentence = sent_gens[idx][0]
-        # Its generated variants
-        generated_sentences = sent_gens[idx][1]
-        # Check that there were some results
-        if generated_sentences:
-            # Reconstruct new tweet sentence from original and generated
-            for sample_idx, generated in enumerate(generated_sentences):
-                # In case there were more generated possibilities in later sentences
-                # than the earlier ones this skips them
-                if idx > 0 and sample_idx > len(gen_samples) - 1: continue
-                sample = regenerate_tweet(sentence, generated)
-                # Append further sentences to new tweet, if any
-                if idx > 0:
-                    gen_samples[sample_idx] += ' ' + sample
-                else:
-                    gen_samples.append(sample)
-                # In some cases generation might produce fewer variants to later
-                # sentences and this just appends one currently used to others.
-                if len(generated_sentences) < len(gen_samples):
-                    for dup_idx in range(sample_idx + 1, len(gen_samples)):
-                        gen_samples[dup_idx] += ' ' + sample
-        else:
-            # If evaluation has not accepted any choices use the original sentence
-            print('Evaluation has failed')
-            gen_samples.append(sentence)
-
-    final_res = external_evaluation(gen_samples)
-
+    for sent_cands in generation_combinations:
+        res += ". ".join(sent_cands) + "\n"
+    # Perform internal evaluation although it does not presently affect anything
+    # save that it takes a fixed size of samples from generated
+    sampled = internal_evaluation(res, tweet)
+    final_res = external_evaluation(sampled)
     return final_res
 
 def regenerate_tweet(tweet_sentence, generated_sentence):
@@ -167,7 +118,7 @@ def regenerate_tweet(tweet_sentence, generated_sentence):
 def external_evaluation(top_candidates):
     use_quota = False  # change to True only when all other blocks of this software is done
     if not use_quota:
-        return top_candidates
+        return top_candidates[-1]
     print("\t-Starting external evaluation for {} candidate Tweets".format(len(top_candidates)))
     lcleval = Evaluator()
     scored_top = [(t, lcleval.external_evaluation(t)) for (t, s) in top_candidates]
@@ -178,7 +129,9 @@ def internal_evaluation(generated, original_tweet):
     print("\t-Starting internal evaluation")
     lcleval = Evaluator()
     # Evaluate original tweet
-    org_eval = lcleval.value_evaluation_for_words(original_tweet)
+    org_novelty = lcleval.novelty_evaluation(original_tweet)
+    org_res = lcleval.value_evaluation_for_words(original_tweet)
+    org_eval = (-org_res) + org_novelty
 
     # Split generated tweets to a list
     tweets = generated.split('\n')[:-1]  # remove the last \n
@@ -189,14 +142,21 @@ def internal_evaluation(generated, original_tweet):
         res = lcleval.value_evaluation_for_words(atweet)
         novelty = lcleval.novelty_evaluation(atweet)
         score = (-res) + novelty  # The lower the score, the better
-        if res > org_eval:
+        if score < org_eval:
             rtweets.append((atweet, score))
     print("\t-Evaluation done")
     # Sort collected tweets in order based on their score
     rtweets = sorted(rtweets, key=lambda x: x[1], reverse=False)
     # Select sample of them
-    sampled = rtweets[:10] # take max top-10
+    sampled = rtweets[:10]  # take max top-10
     return sampled
+
+
+def format_output(original_tweet, generated_tweet):
+    tb1 = TextBlob(original_tweet).sentences
+    tb2 = TextBlob(generated_tweet).sentences
+
+    return "".join([regenerate_tweet(str(os), str(gs)) for (os, gs)in zip(tb1, tb2)])
 
 
 def main():
@@ -204,8 +164,8 @@ def main():
     print(tweet)
     output = generate_text(tweet)
     # Display generated texts
-    for t in output:
-        print(t)
+    formatted_output = format_output(tweet, output[0])
+    print(formatted_output)
 
 
 if __name__ == '__main__':
